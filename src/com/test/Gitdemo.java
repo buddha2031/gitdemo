629a7ca25cf0cd05e97a17193f2f862d24b12d26
Buddha <bostonbuddha08@gmail.com>
2022-03-23 09:46:30 -0400

    this is second

--- src/com/test/Gitdemo.java
+++ src/com/test/Gitdemo.java
@@ -3,5 +3,6 @@ package com.test;
 public class Gitdemo {
     public static void main(String[] args) {
         System.out.println("Boston Buddha");
+        System.out.println("welcome to Massachusetts");
     }
 }
